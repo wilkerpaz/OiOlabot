648cddffa83b0e794777c8a251d64e8fad17cfdb
wilkerpaz <wilkerpaz@hotmail.com>
2020-05-03 17:59:53 -0300

    first_commit

--- bot.py
+++ bot.py
@@ -130,8 +130,9 @@ def _introduce(client, update):
               emojize(':grinning_face:'))
     update.reply_text(text=text, quote=False, parse_mode='html')
 
-    if client.get_me().first_name == 'EvangelhodoDia_bot':
-        user_name = update.from_user.first_name
+    if client.get_me().username == 'LiturgiaDiaria_bot':
+        user_name = '@' + update.chat.username if update.chat.username else \
+            '@' + update.from_user.username if update.from_user.username else update.from_user.first_name
         url = 'http://feeds.feedburner.com/evangelhoddia/dia'
         db.set_url_to_group(chat_id=invited, user_id=chat_id, user_name=user_name, url=url)
 
--- ltd_bot.py
+++ ltd_bot.py
@@ -130,11 +130,10 @@ def _introduce(client, update):
               emojize(':grinning_face:'))
     update.reply_text(text=text, quote=False, parse_mode='html')
 
-    logger.info('Start bot  (%s)' % client.get_me().username)
     if client.get_me().username == 'LiturgiaDiaria_bot':
-        user_name = '@' + update.from_user.username if update.from_user.username else update.from_user.first_name
+        user_name = '@' + update.chat.username if update.chat.username else \
+            '@' + update.from_user.username if update.from_user.username else update.from_user.first_name
         url = 'http://feeds.feedburner.com/evangelhoddia/dia'
-        print(invited, chat_id, user_name, url)
         db.set_url_to_group(chat_id=invited, user_id=chat_id, user_name=user_name, url=url)
 
 
